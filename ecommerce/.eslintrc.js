efbb8d2fbf90cc7c01cd0bf56856f61418629e27
Anastasiya Andronova <anastasiya5.88@mail.ru>
2023-08-05 11:32:11 +0300

    refactor: improve config files

--- ecommerce/.eslintrc.js
+++ ecommerce/.eslintrc.js
@@ -1,11 +1,29 @@
-module.exports = {
+{
     plugins: ['prettier', 'import', '@typescript-eslint'],
     extends: [
+        'react-app',
+        'shared-config',
         'airbnb-base',
         'airbnb-typescript/base',
         'plugin:@typescript-eslint/recommended',
         'plugin:prettier/recommended',
         'prettier',
+    ],
+    rules: {
+        'additional-rule': 'warn',
+        'no-debugger': 'off',
+        'no-console': 0,
+        'class-methods-use-this': 'off',
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/no-inferrable-types': 'off',
+    },
+    overrides: [
+        {
+            files: ['**/*.ts?(x)'],
+            rules: {
+                'additional-typescript-only-rule': 'warn',
+            },
+        },
     ],
     env: {
         browser: true,
@@ -20,11 +38,5 @@ module.exports = {
         project: './tsconfig.json',
         tsconfigRootDir: __dirname,
     },
-    rules: {
-        'no-debugger': 'off',
-        'no-console': 0,
-        'class-methods-use-this': 'off',
-        '@typescript-eslint/no-explicit-any': 2,
-        '@typescript-eslint/no-inferrable-types': 'off',
-    },
-};
+}
+
