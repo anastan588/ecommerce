1062f9fff5128c34b6618eb933db20768abeb340
Jjjulietta <jjjuliettai@gmail.com>
2023-08-16 19:21:15 +0300

    feat: add file with constants

---
+++ ecommerce/src/components/login_page/data.js
@@ -0,0 +1,7 @@
+const ADMIN_CLIENT_ID = 'X7xMQJdEw5S3mkVSv8ZKdGy1';
+const ADMIN_CLIENT_SECRET = 'eSSKTHOweSI0n3bIOuLjXCzjzqwjHoPs';
+const CTP_AUTH_URL = 'europe-west1.gcp';
+const CTP_API_URL = 'https://api.europe-west1.gcp.commercetools.com';
+const CTP_PROJECT_KEY = 'rsschool-final-task-stage2';
+
+export { ADMIN_CLIENT_ID, ADMIN_CLIENT_SECRET, CTP_AUTH_URL, CTP_API_URL, CTP_PROJECT_KEY };
