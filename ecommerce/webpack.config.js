da07a7ad513f72331cea18fd07aa8a7f4abf413d
anastan588 <anastasiya5.88@mail.ru>
2023-08-20 23:31:24 +0300

    feat: add webpackconfig

--- ecommerce/webpack.config.js
+++ ecommerce/webpack.config.js
@@ -1,5 +1,8 @@
 const webpack = require('webpack');
 const dotenv = require('dotenv');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 const env = dotenv.config().parsed;
 const envKeys = Object.keys(env).reduce((prev, next) => {
@@ -12,6 +15,17 @@ module.exports = {
         new webpack.DefinePlugin(envKeys),
         new webpack.NormalModuleReplacementPlugin(/node:/, (resource) => {
             resource.request = resource.request.replace(/^node:/, '');
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+        }),
+        new FileManagerPlugin({
+            events: {
+                onStart: {
+                    delete: ['build'],
+                },
+            },
         }),
     ],
     externalsPresets: {
