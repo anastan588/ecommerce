378815785d158187180d9abd49d3cfe7c660c62b
Jjjulietta <jjjuliettai@gmail.com>
2023-08-20 21:03:03 +0300

    fix: change app test file

--- ecommerce/src/App.test.tsx
+++ ecommerce/src/App.test.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+window.matchMedia = (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+});
+
 test('renders learn react link', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
+    const linkElement = screen.getByText('Registrations');
     expect(linkElement).toBeInTheDocument();
 });
