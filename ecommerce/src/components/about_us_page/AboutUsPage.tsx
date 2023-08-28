ad3ecec6b4a1620a7410ec39481799f68162bee7
Aliaksandr Fedukovich <1632637@mail.ru>
2023-08-28 12:10:36 +0300

    fix: fix error with attributes plants

--- ecommerce/src/components/about_us_page/AboutUsPage.tsx
+++ ecommerce/src/components/about_us_page/AboutUsPage.tsx
@@ -1,66 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
 import { Avatar, Card } from 'antd';
 import { apiRoot } from '../login_page/createClient';
 
 const { Meta } = Card;
 
 type ImagesType = {
-    url: string,
-    dimensions: {'w': number, 'h': number}
-}
+    url: string;
+    dimensions: { w: number; h: number };
+};
 
-type AttributesPlants = {'name': string, 'value': string}
+type AttributesPlants = { name: string; value: string };
 
 type MasterVariantType = {
-    images: ImagesType[],
+    images: ImagesType[];
     attributes: AttributesPlants[];
-
-}
+};
 
 type MasterDataCurrent = {
-    masterVariant: MasterVariantType,
-    name: {'en-US': string, 'ru': string},
-    description: {'en-US': string, 'ru': string},
-
-}
+    masterVariant: MasterVariantType;
+    name: { 'en-US': string; ru: string };
+    description: { 'en-US': string; ru: string };
+};
 
 type MasterDataType = {
-    current: MasterDataCurrent,
-    
-}
+    current: MasterDataCurrent;
+};
 
 type ProductType = {
-    id?: string,
-    key?: string,
-    masterData?: MasterDataType,
-}
-
+    id?: string;
+    key?: string;
+    masterData?: MasterDataType;
+};
 
 const AboutUsPage = () => {
-    const state = useState({})
+    const state = useState({});
     const product: ProductType = state[0];
     const setProduct = state[1];
 
-    useEffect(()=> {
-        apiRoot.products().get().execute().then(body => {
-            console.log(body.body.results);
-            setProduct(body.body.results[0]);
-        })
-    }, [])
-    
+    useEffect(() => {
+        apiRoot
+            .products()
+            .get()
+            .execute()
+            .then((body) => {
+                console.log(body.body.results);
+                setProduct(body.body.results[0]);
+            });
+    }, []);
 
-
-
-    
-    
     /* getProducts().then((body) => {
         console.log(body);
     }); */
-
-
 
     /* const getProducts = () => {
         return apiRoot.products().get().execute();
@@ -70,76 +62,36 @@ const AboutUsPage = () => {
         console.log(body);
     }); */
 
-
-
-
-
-
     console.log('product-id');
     console.log(product.id);
     console.log(product.key);
-    
-    
+
     const pathImage = product.masterData?.current.masterVariant.images[0].url;
     const titlePlants = product.masterData?.current.name.ru;
     const descriptionPlants = product.masterData?.current.description.ru;
     const attributesPlants = product.masterData?.current.masterVariant.attributes as AttributesPlants[];
 
-    
-
     console.log(attributesPlants);
 
-    console.log(pathImage)
+    console.log(pathImage);
 
-
-
-
-
-
-
-    return(
+    return (
         <div>
             <h2 className="page_title main">About1234</h2>
 
-            
-            
+            <Card style={{ width: 500 }} cover={<img alt="example" src={pathImage} />}>
+                <Meta title={titlePlants} description={descriptionPlants} />
 
-            <Card
-                style={{ width: 500 }}
+
+
 
                 
-                
-                cover={
-                <img
-                    alt="example"
-                    src={pathImage}
-                />
-                }
-            >
-                <Meta
-                    title={titlePlants}
-                    description={descriptionPlants}
-                />
-
-                <ul>
-                    {attributesPlants.map((item) => <li>{item.name}: {item.value}</li>)}
-                </ul>
             </Card>
-
-
-
-
-
-
-
-            
         </div>
-        
-    ) 
+    );
 };
 
 export default AboutUsPage;
-
 
 /* const AboutUsPage1 = () => {
     return <h2 className="page_title main">About us page</h2>;
