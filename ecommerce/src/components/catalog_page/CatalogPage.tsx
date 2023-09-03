4dd807935d117bbe0d78223c8c1089dd15493291
Jjjulietta <jjjuliettai@gmail.com>
2023-09-03 15:57:59 +0300

    feat: implement a catalog filter panel

--- ecommerce/src/components/catalog_page/CatalogPage.tsx
+++ ecommerce/src/components/catalog_page/CatalogPage.tsx
@@ -1,7 +1,127 @@
-import React from 'react';
+import { observer } from 'mobx-react-lite';
+import React, { useContext, useEffect } from 'react';
+import { Context } from '../..';
+import Catalog from './Catalog';
+import './catalog.css';
+// import CategoryBar from './CategoryBar';
+import { productsRes, productsType, categories, attributesList } from './requests';
+import TypesBar from './TypesBar';
+import { apiRoot } from './ClientBuilderView';
+import Sorting from './filter_components/sorting';
+import SortingAl from './filter_components/sortingAlfabet';
+import FilterBar from './filterBar';
+import { AttributeType } from './productsStore';
 
-const CatalogPage = () => {
-    return <h2 className="page_title main">Catalog page</h2>;
-};
+const CatalogPage = observer(() => {
+    const products = useContext(Context);
+    useEffect(() => {
+        productsType().then(({ body }) => {
+            console.log(body);
+            const arr = body.results.map((item) => {
+                // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+                return { id: item.id, name: item.name };
+            });
+            products.products.setTypes(arr);
+        });
+        productsRes().then(({ body }) => {
+            const arr = body.results.map((item) => {
+                return {
+                    id: item.id,
+                    name: item.masterData.current.name,
+                    categoriesId: item.masterData.current.categories[0].id,
+                    attributes: item.masterData.current.masterVariant.attributes,
+                    description: item.masterData.current.description,
+                    images: item.masterData.current.masterVariant.images,
+                    prices: item.masterData.current.masterVariant.prices,
+                };
+            });
+            console.log(arr);
+            products.products.setProducts(arr);
+        });
+        categories().then(({ body }) => {
+            console.log(body);
+            const category = body.results
+                .filter((item) => item.parent)
+                .map((item) => {
+                    return { id: item.id, name: item.name.en };
+                });
+            console.log(category);
+            products.products.setCategory(category);
+        });
+        attributesList().then(async ({ body }) => {
+            if (body) {
+                const attr = body.results[0].masterData.current.masterVariant.attributes?.map((item) => item.name);
+                console.log(attr);
+                // eslint-disable-next-line @typescript-eslint/no-shadow
+                if (attr) {
+                    const attrValues = await Promise.all(
+                        attr.map(async (item) => {
+                            const res = await apiRoot
+                                .productProjections()
+                                .search()
+                                .get({ queryArgs: { filter: `variants.attributes.${item}:exists` } })
+                                .execute()
+                                // eslint-disable-next-line @typescript-eslint/no-shadow,
+                                .then(({ body }): AttributeType | undefined => {
+                                    const attributesVal = body.results.map((value) => {
+                                        return value.masterVariant.attributes;
+                                    });
+                                    // console.log(attributesVal);
+                                    const valAttr = attributesVal.map((v) => {
+                                        return v?.find((i) => i.name === item);
+                                    });
+                                    if (valAttr) {
+                                        const ar: string[] = valAttr.map((i) => i?.value);
+                                        // console.log(ar);
+                                        const s: Set<string> = new Set();
+                                        ar.forEach((i) => s.add(i));
+                                        return { name: item, value: Array.from(s) };
+                                    }
+                                    return undefined;
+                                    // eslint-disable-next-line array-callback-return
+                                    /* const arr: string[][] | undefined | unknown[] = body.results.map((value) => {
+                                        if (value.masterVariant.attributes) {
+                                            value.masterVariant.attributes
+                                                ?.filter((it) => it.name === `${item}`)
+                                                .map((val) => val.value);
+                                        }
+                                    });
+                                    console.log(arr);
+                                    const set = new Set();
+                                    arr.flat().forEach((i) => set.add(i));
+                                    return { name: item, value: Array.from(set) }; */
+                                });
+                            // console.log(res);
+                            return res;
+                        })
+                    );
+                    console.log(attrValues);
+                    const c: AttributeType[] = [];
+                    attrValues.forEach((item) => {
+                        if (item) {
+                            c.push(item);
+                        }
+                    });
+                    products.products.setAttributes(c);
+                }
+            }
+        });
+    }, []);
+
+    return (
+        <div className="catalog">
+            <h2 className="page_title main">Catalog</h2>
+            <div className="type-container">
+                <TypesBar />
+                <Sorting />
+                <SortingAl />
+            </div>
+            <div className="catalog_container">
+                <FilterBar />
+                <Catalog />
+            </div>
+        </div>
+    );
+});
 
 export default CatalogPage;
---
+++ ecommerce/src/components/catalog_page/filterBar.tsx
@@ -0,0 +1,74 @@
+import React, { useContext } from 'react';
+import type { CollapseProps } from 'antd';
+import { Collapse } from 'antd';
+import { observer } from 'mobx-react-lite';
+// import Color from './filter_components/Color';
+import Categories from './filter_components/Categories';
+import { Context } from '../..';
+import { Attributeitem } from './filter_components/Attributes';
+
+// const attributes = [ <Color/>, ]
+
+/* const itemsNest: CollapseProps['items'] = [
+    {
+        key: '1',
+        label: 'This is panel nest panel',
+        children: <AttributesBar />,
+    },
+    {
+        key: '2',
+        label: 'This is panel nest panel',
+        children: <p>{text}</p>,
+    },
+]; */
+
+/* const items: CollapseProps['items'] = [
+    {
+        key: '1',
+        label: 'This is panel header 1',
+        children: <Collapse defaultActiveKey="1" items={itemsNest} />,
+    },
+    {
+        key: '2',
+        label: 'This is panel header 2',
+        children: <AttributesBar />,
+    },
+    {
+        key: '3',
+        label: 'This is panel header 3',
+        children: <Categories />,
+    },
+]; */
+
+const FilterBar: React.FC = observer(() => {
+    const products = useContext(Context);
+    const itemsNest: CollapseProps['items'] = products.products.attributes.map((item, i) => {
+        return { key: i, label: item.name, children: <Attributeitem key={item.name} item={item} /> };
+    });
+    console.log(itemsNest);
+    const items: CollapseProps['items'] = [
+        {
+            key: '1',
+            label: 'Выбрать категории',
+            children: <Categories />,
+        },
+        {
+            key: '2',
+            label: 'Выбрать характеристики товара',
+            children: <Collapse defaultActiveKey="1" items={itemsNest} />,
+        },
+        /* {
+            key: '2',
+            label: 'This is panel header 2',
+            children: <AttributesBar />,
+        }, */
+    ];
+    // const itemNest = products.products.attributes.map((item, i) => { return {key: i, label: item, children: }}))
+    const onChange = (key: string | string[]) => {
+        console.log(key);
+    };
+
+    return <Collapse className="filter_bar" onChange={onChange} items={items} />;
+});
+
+export default FilterBar;
