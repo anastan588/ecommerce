7c90946dd53437f5d0cab31843f44fb15f56fb51
Jjjulietta <jjjuliettai@gmail.com>
2023-09-09 15:18:01 +0300

    refactor: delete file and edit code

--- ecommerce/src/index.tsx
+++ ecommerce/src/index.tsx
@@ -6,11 +6,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Store from './components/login_page/store';
 import { ProductsStore } from './components/catalog_page/productsStore';
+import { BasketStore } from './components/basket_page/BasketStore';
 
 const store = new Store();
 const products = new ProductsStore();
+const cart = new BasketStore();
 // eslint-disable-next-line import/prefer-default-export
-export const Context = createContext({ store, products });
+export const Context = createContext({ store, products, cart });
 // export const ProductsContext = createContext({ products });
 
 const element = document.querySelector('#root');
@@ -18,7 +20,7 @@ const element = document.querySelector('#root');
 if (element) {
     const root = ReactDOM.createRoot(element);
     root.render(
-        <Context.Provider value={{ store, products }}>
+        <Context.Provider value={{ store, products, cart }}>
             <React.StrictMode>
                 <App />
             </React.StrictMode>
