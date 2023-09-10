209bbb7cce47c5014964d1d85dbf05f1433a6787
Jjjulietta <jjjuliettai@gmail.com>
2023-09-10 11:09:02 +0300

    feat: implement a product removal button

--- ecommerce/src/components/product_page/Button_product.tsx
+++ ecommerce/src/components/product_page/Button_product.tsx
@@ -6,6 +6,7 @@ import './productStyles.css';
 import ButtonCarts from '../catalog_page/Cart';
 import {
     getCartsAnonimus,
+    getCartsAuth,
     getCartsProduct,
     getProductById,
     removeProductItemAnonim,
@@ -20,61 +21,65 @@ const ButtonProduct: React.FC<{ item: Obj }> = (props) => {
     const { store, cart } = useContext(Context);
     const [style, setStyle] = useState('button_carts');
     const [value3, setValue3] = useState('В корзину');
-    const [valueDel, setValueDel] = useState();
+    const [disabled, setDisabled] = useState(true);
     const [size, setSize] = useState<SizeType>('middle'); // default is 'middle'
     let value = cart.getProducts().find((val) => val.productId === props.item.id);
     console.log(props.item.id);
     console.log(value?.id);
-    /* useEffect(() => {
+    useEffect(() => {
         value = cart.getProducts().find((val) => val.productId === props.item.id);
-        console.log(value)
-    }, [value]); */
+        console.log(value);
+        if (value) {
+            setDisabled(false);
+        }
+    }, [value]);
     const handleEvent = async () => {
         if (store.isAuth) {
             const tokenStore = getLocalStorage();
             console.log(tokenStore);
             const { refreshToken } = tokenStore;
-            const { cartId, version } = cart.getCart()[0];
-            console.log(cartId);
-            if (value) {
-                const id = value?.id;
-                const itemsCart = await removeProductItemCastomer(refreshToken, cartId, id, version);
-                console.log(itemsCart);
-                // const cartValues = (await getCartsProduct(cartId)).body.lineItems;
-                // console.log(cartValues);
-                if (itemsCart && !itemsCart.find((item) => item.productId === props.item.id))
-                    cart.setProducts(itemsCart);
-                setValue3('В корзину');
-                setStyle('button_carts');
-                // value = undefined;
+            const activeCart = await getCartsAuth(refreshToken);
+            if (activeCart) {
+                const { cartId, version } = activeCart;
+                console.log(cartId);
+                if (value) {
+                    const id = value?.id;
+                    const itemsCart = await removeProductItemCastomer(refreshToken, cartId, id, version);
+                    console.log(itemsCart);
+                    // const cartValues = (await getCartsProduct(cartId)).body.lineItems;
+                    // console.log(cartValues);
+                    if (itemsCart && !itemsCart.find((item) => item.productId === props.item.id))
+                        cart.setProducts(itemsCart);
+                    setValue3('В корзину');
+                    setStyle('button_carts');
+                    setDisabled(true);
+                }
             }
         } else {
             const getCartsAnonim = await getCartsAnonimus();
             console.log(getCartsAnonim);
-            if (getCartsAnonim) {
+            if (getCartsAnonim && value) {
+                const id = value?.id;
                 console.log('cart');
                 console.log(getCartsAnonim.id);
                 console.log(props.item.id);
                 console.log(getCartsAnonim.version);
-                const itemsCart = await removeProductItemAnonim(
-                    getCartsAnonim.id,
-                    props.item.id,
-                    getCartsAnonim.version
-                );
+                const itemsCart = await removeProductItemAnonim(getCartsAnonim.id, id, getCartsAnonim.version);
                 console.log(itemsCart);
                 if (itemsCart) cart.setProducts(itemsCart);
                 setValue3('В корзину');
                 setStyle('button_carts');
-                // value = undefined;
+                setDisabled(true);
             }
-        } value = undefined;
+        }
+        value = undefined;
     };
 
     return (
         <>
             <div className="buttons_product">
                 <ButtonCarts item={props.item} />
-                <Button type="primary" className="button-product" size={size} onClick={handleEvent} disabled={!value}>
+                <Button type="primary" className="button-product" size={size} onClick={handleEvent} disabled={disabled}>
                     Удалить из корзины
                 </Button>
             </div>
